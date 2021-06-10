b647a20a762ecb9117cb9acd77f3e87f70e3f2b7
Ross Squires <ross.squires@gmail.com>
2021-06-09 21:03:50 -0400

    Rearrange order

--- Game.cs
+++ Game.cs
@@ -4,48 +4,48 @@ using System;
 
 public class Game : Godot.YSort
 {
-	public State state;
+    public State state;
 
-	public override void _Ready()
-	{
-		var hero = new Entity(
-			new Player(),
-			new Position(X: 50, Y: 50),
-			new Scale(3, 3),
-			new Sprite("res://resources/tiles/tile072.png"));
+    public override void _Ready()
+    {
+        var hero = new Entity(
+            new Player(),
+            new Position(X: 50, Y: 50),
+            new Scale(3, 3),
+            new Sprite("res://resources/tiles/tile072.png"));
 
-		var potion = new Entity(
-			new Position(X: 200, Y: 200),
-			new Collide(),
-			new Click(new AddRotation(36f)),
-			new Scale(2, 2),
-			new Sprite("res://resources/tiles/tile570.png"));
+        var potion = new Entity(
+            new Position(X: 200, Y: 200),
+            new Collide(),
+            new Click(new AddRotation(36f)),
+            new Scale(2, 2),
+            new Sprite("res://resources/tiles/tile570.png"));
 
-		state = new State() {
-			{ "hero", hero },
-			{ "potion", potion }
-		};
-	}
+        state = new State() {
+            { "hero", hero },
+            { "potion", potion }
+        };
+    }
 
-	public void _Collision(Area2D area, string id)
-	{
-		Console.WriteLine($"{id}");
-	}
+    public override void _Input(InputEvent @event)
+    {
+        state = Input.System(state, this, @event);
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-		state = Input.System(state, this, @event);
-	}
+    public override void _Process(float delta)
+    {
+        state = Movement.System(state);
+        state = Items.System(state);
+        state = Renderer.System(state, this);
+    }
 
-	public void _Event(string id, GodotWrapper ev)
-	{
-		state = Event.System(state, id, ev.Get<Component>());
-	}
+    public void _Event(string id, GodotWrapper ev)
+    {
+        state = Event.System(state, id, ev.Get<Component>());
+    }
 
-	public override void _Process(float delta)
-	{
-		state = Movement.System(state);
-		state = Items.System(state);
-		state = Renderer.System(state, this);
-	}
+    public void _Collision(Area2D area, string id)
+    {
+        Console.WriteLine($"{id}");
+    }
 }
