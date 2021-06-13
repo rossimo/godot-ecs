849b16d427d6f80818e6c5eacf045c0f428e7c35
Ross Squires <39342458+rsquires@users.noreply.github.com>
2021-06-13 19:43:29 -0400

    Inventory toString()

--- Items.cs
+++ Items.cs
@@ -1,14 +1,15 @@
 using Ecs;
+using System.Linq;
 using System.Collections.Generic;
 
 public record Inventory : Component
 {
-    public IEnumerable<Component> Items = new List<Component>();
+    public Component[] Items = new Component[] { };
 
     public Inventory() { }
 
     public Inventory(IEnumerable<Component> items)
-        => (Items) = (items);
+        => (Items) = (items.ToArray());
 }
 
 public record AddItem(Component Item, string Target = null, bool TargetOther = false) : Command(Target, TargetOther);
