054493b5a388765762848f836e61aa86eae2a5c6
Ross Squires <39342458+rsquires@users.noreply.github.com>
2021-07-12 21:14:10 -0400

    Remove redundant generics

--- Game.cs
+++ Game.cs
@@ -10,37 +10,37 @@ public class Game : Godot.YSort
     public override void _Ready()
     {
         var player = World.CreateEntity();
-        player.Set<Player>(new Player());
-        player.Set<Speed>(new Speed { Value = 2.5f });
-        player.Set<Position>(new Position { X = 50, Y = 50 });
-        player.Set<Scale>(new Scale { X = 3, Y = 3 });
-        player.Set<Area>(new Area());
-        player.Set<Sprite>(new Sprite { Image = "res://resources/tiles/tile072.png" });
-        player.Set<Collision>(new Collision());
+        player.Set(new Player());
+        player.Set(new Speed { Value = 2.5f });
+        player.Set(new Position { X = 50, Y = 50 });
+        player.Set(new Scale { X = 3, Y = 3 });
+        player.Set(new Area());
+        player.Set(new Sprite { Image = "res://resources/tiles/tile072.png" });
+        player.Set(new Collision());
 
         var fire = World.CreateEntity();
-        fire.Set<Position>(new Position { X = 400, Y = 200 });
-        fire.Set<Area>(new Area());
-        fire.Set<Collision>(new Collision());
-        fire.Set<CollisionEvent>(new CollisionEvent(
+        fire.Set(new Position { X = 400, Y = 200 });
+        fire.Set(new Area());
+        fire.Set(new Collision());
+        fire.Set(new CollisionEvent(
             new Add<Flash>(new Flash { Color = new Color { Red = 2f, Green = 2f, Blue = 0f } }),
             new Add<Flash>(new Flash { Color = new Color { Red = 1f, Green = 0f, Blue = 0f } }, Target.Other)
         ));
-        fire.Set<Scale>(new Scale { X = 2, Y = 2 });
-        fire.Set<Sprite>(new Sprite { Image = "res://resources/tiles/tile495.png" });
+        fire.Set(new Scale { X = 2, Y = 2 });
+        fire.Set(new Sprite { Image = "res://resources/tiles/tile495.png" });
 
         var button = World.CreateEntity();
-        button.Set<Position>(new Position { X = 300, Y = 300 });
-        button.Set<Area>(new Area());
-        button.Set<AreaEnterEvent>(new AreaEnterEvent(
+        button.Set(new Position { X = 300, Y = 300 });
+        button.Set(new Area());
+        button.Set(new AreaEnterEvent(
             new Add<Flash>(new Flash { Color = new Color { Red = 0.1f, Green = 0.1f, Blue = 0.1f } }),
             new AddEntity(Potion, 11)
         ));
-        button.Set<Scale>(new Scale { X = 2, Y = 2 });
-        button.Set<Sprite>(new Sprite { Image = "res://resources/tiles/tile481.png" });
+        button.Set(new Scale { X = 2, Y = 2 });
+        button.Set(new Sprite { Image = "res://resources/tiles/tile481.png" });
 
         var events = World.CreateEntity();
-        events.Set<EventQueue>(new EventQueue());
+        events.Set(new EventQueue());
 
         var input = World.CreateEntity();
 
