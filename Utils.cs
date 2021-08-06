679924d1c8f212d1e69cad09043557f8ed0628af
Ross Squires <ross.squires@gmail.com>
2021-08-06 19:41:08 -0400

    Flatten structs for metadata

--- Utils.cs
+++ Utils.cs
@@ -1,16 +1,71 @@
+using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class Utils
 {
-    public static T[] ToArray<T>(this Godot.Collections.Array godotArray)
+    public static T[] ToArray<T>(this Godot.Collections.Array array)
     {
         var list = new List<T>();
 
-        foreach (T element in godotArray)
+        foreach (T element in array)
         {
             list.Add(element);
         }
 
         return list.ToArray();
+    }
+
+    public static Dictionary<string, object> ToFieldMap(this object obj)
+    {
+        var type = obj.GetType();
+        var metadata = new Dictionary<string, object>();
+
+        foreach (var fieldInfo in type.GetFields())
+        {
+            var key = fieldInfo.Name.ToLower();
+            var value = fieldInfo.GetValue(obj);
+            var fieldType = fieldInfo.FieldType;
+
+            if (fieldType.IsPrimitive || value is string)
+            {
+                metadata.Add(key, value);
+            }
+            else
+            {
+                metadata.Add(key, value.ToFieldMap());
+            }
+        }
+
+        return metadata;
+    }
+
+    public static Dictionary<string, object> ToFlat(this Dictionary<string, object> input, string sep, string prefix = "")
+    {
+        var output = new Dictionary<string, object>();
+
+        foreach (var entry in input)
+        {
+            var key = entry.Key;
+            var value = entry.Value;
+            var name = String.Join(sep, new[] { prefix, key }.Where(el => el.Length > 0));
+
+            if (value is Dictionary<string, object> inner)
+            {
+                foreach (var innerEntry in ToFlat(inner, sep, name))
+                {
+                    var innerKey = innerEntry.Key;
+                    var innerValue = innerEntry.Value;
+
+                    output.Add(innerKey, innerValue);
+                }
+            }
+            else
+            {
+                output.Add(name, value);
+            }
+        }
+
+        return output;
     }
 }
--- addons/ecsplugin/EcsPlugin.cs
+++ addons/ecsplugin/EcsPlugin.cs
@@ -1,121 +1,123 @@
 using Godot;
-using Godot.Collections;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 [Tool]
 public class EcsPlugin : EditorPlugin
 {
-	private Control dock;
-	private List<Type> components = new List<Type>();
-	private List<Type> available = GetComponents().ToList();
-	private Node current;
+    private Control dock;
+    private List<Type> components = new List<Type>();
+    private List<Type> available = GetComponents().ToList();
+    private Node current;
 
-	public override void _EnterTree()
-	{
-		dock = new Panel() { Name = "Components" };
-		AddControlToDock(DockSlot.RightUl, dock);
+    public override void _EnterTree()
+    {
+        dock = new Panel() { Name = "Components" };
+        AddControlToDock(DockSlot.RightUl, dock);
 
-		RenderComponents();
+        RenderComponents();
 
-		var selector = GetEditorInterface().GetSelection();
-		selector.Connect("selection_changed", this, nameof(SelectedNode));
+        var test = new Move() { Destination = new Position() { X = 10, Y = 30 } };
+        foreach (var entry in test.ToFieldMap().ToFlat("/"))
+        {
+            Console.WriteLine($"{entry.Key} = {entry.Value}");
+        }
 
-		current = selector.GetSelectedNodes().ToArray<Node>()?.FirstOrDefault();
-	}
+        var selector = GetEditorInterface().GetSelection();
+        selector.Connect("selection_changed", this, nameof(SelectedNode));
 
-	public override void _ExitTree()
-	{
-		RemoveControlFromDocks(dock);
-		dock.QueueFree();
-	}
+        current = selector.GetSelectedNodes().ToArray<Node>()?.FirstOrDefault();
+    }
 
-	public void SelectedNode()
-	{
-		var selector = GetEditorInterface().GetSelection();
-		current = selector.GetSelectedNodes().ToArray<Node>()?.FirstOrDefault();
-	}
+    public override void _ExitTree()
+    {
+        RemoveControlFromDocks(dock);
+        dock.QueueFree();
+    }
 
-	public void RenderComponents()
-	{
-		foreach (Node child in dock.GetChildren())
-		{
-			dock.RemoveChild(child);
-			child.QueueFree();
-		}
+    public void SelectedNode()
+    {
+        var selector = GetEditorInterface().GetSelection();
+        current = selector.GetSelectedNodes().ToArray<Node>()?.FirstOrDefault();
+    }
 
+    public void RenderComponents()
+    {
+        foreach (Node child in dock.GetChildren())
+        {
+            dock.RemoveChild(child);
+            child.QueueFree();
+        }
 
-		if (current != null)
-		{
-			foreach (var metadata in current.GetMetaList())
-			{
-				var path = metadata.Split('/');
-				if (path.FirstOrDefault() != "component") continue;
+        var layout = new VBoxContainer();
+        layout.AnchorRight = 1;
+        dock.AddChild(layout);
 
-				var name = path[0];
-				var component = GetComponents().FirstOrDefault(component => component.Name.ToLower() == name.ToLower());
-				if (component == null) continue;
-			}
-		}
+        foreach (var metadata in current?.GetMetaList() ?? new string[] { })
+        {
+            var path = metadata.Split('/');
+            if (path.Length < 2) continue;
 
-		var layout = new VBoxContainer();
-		layout.AnchorRight = 1;
-		dock.AddChild(layout);
+            var prefix = path[0];
+            if (path[0] != "component") continue;
 
-		foreach (var component in components)
-		{
-			var componentLayout = new HBoxContainer();
-			componentLayout.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
-			layout.AddChild(componentLayout);
+            var name = path[1];
+            var component = GetComponents()
+                .FirstOrDefault(component => component.Name.ToLower() == name.ToLower());
+            if (component == null) continue;
 
-			var componentLabel = new Label() { Text = component.Name };
-			componentLabel.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
-			componentLabel.SizeFlagsVertical = 0;
-			componentLayout.AddChild(componentLabel);
+            var componentLayout = new HBoxContainer();
+            componentLayout.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
+            layout.AddChild(componentLayout);
 
-			var fieldsLayout = new VBoxContainer();
-			fieldsLayout.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
-			fieldsLayout.SizeFlagsVertical = 0;
-			componentLayout.AddChild(fieldsLayout);
+            var componentLabel = new Label() { Text = component.Name };
+            componentLabel.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
+            componentLabel.SizeFlagsVertical = 0;
+            componentLayout.AddChild(componentLabel);
 
-			foreach (var field in component.GetFields())
-			{
-				var fieldLabel = new Label() { Text = field.Name };
-				fieldsLayout.AddChild(fieldLabel);
-			}
-		}
+            var fieldsLayout = new VBoxContainer();
+            fieldsLayout.SizeFlagsHorizontal = (int)Control.SizeFlags.ExpandFill;
+            fieldsLayout.SizeFlagsVertical = 0;
+            componentLayout.AddChild(fieldsLayout);
 
-		var picker = new OptionButton() { Text = "Add Component" };
-		layout.AddChild(picker);
-		picker.Connect("item_selected", this, nameof(AddComponent));
+            foreach (var fieldInfo in component.GetFields())
+            {
+                var fieldLabel = new Label() { Text = fieldInfo.Name };
+                fieldsLayout.AddChild(fieldLabel);
+            }
+        }
 
-		for (var i = 0; i < available.Count; i++)
-		{
-			var component = available[i];
-			if (components.Contains(component)) continue;
+        var picker = new OptionButton() { Text = "Add Component" };
+        layout.AddChild(picker);
+        picker.Connect("item_selected", this, nameof(AddComponent));
 
-			picker.GetPopup().AddItem(component.Name, i);
-		}
-	}
+        for (var i = 0; i < available.Count; i++)
+        {
+            var component = available[i];
+            if (components.Contains(component)) continue;
 
-	public void AddComponent(int index)
-	{
-		var component = available[index];
-		components.Add(component);
-		components = components.OrderBy(component => component.Name).ToList();
+            picker.GetPopup().AddItem(component.Name, i);
+        }
+    }
 
-		available = GetComponents().Where(component => !components.Contains(component)).ToList();
+    public void AddComponent(int index)
+    {
+        var component = available[index];
+        components.Add(component);
+        components = components.OrderBy(component => component.Name).ToList();
 
-		RenderComponents();
-	}
+        available = GetComponents().Where(component => !components.Contains(component)).ToList();
 
-	public static Type[] GetComponents()
-	{
-		return AppDomain.CurrentDomain.GetAssemblies()
-			.SelectMany(assembly => assembly.GetTypes())
-			.Where(type => type.GetCustomAttributes(typeof(EditorComponent), false)?.Length > 0)
-			.OrderBy(component => component.Name)
-			.ToArray();
-	}
+        RenderComponents();
+    }
+
+    public static Type[] GetComponents()
+    {
+        return AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.GetCustomAttributes(typeof(EditorComponent), false)?.Length > 0)
+            .OrderBy(component => component.Name)
+            .ToArray();
+    }
 }
